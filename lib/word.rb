a71940dad7202575c5fa0d6adf16d5258f869f31
J.P. Barela <barela@jpbarela.com>
2016-07-22 11:38:01 -0600

    add solution

--- lib/word.rb
+++ lib/word.rb
@@ -6,4 +6,18 @@ class Word
   def original_word
     return @original_word.downcase
   end
+
+  def piglatinize
+  	word_array = original_word.chars
+  	first_letters = []
+  	first_letters.push(word_array.shift) unless ['a', 'e','i','o','u'].include? word_array.first
+  	if first_letters.empty?
+  		original_word + 'way'	
+  	else
+  		while !['a', 'e','i','o','u'].include? word_array.first
+  			first_letters.push(word_array.shift) 
+  		end
+  		word_array.join + first_letters.join + 'ay'
+  	end
+  end
 end
