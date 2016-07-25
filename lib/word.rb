03764d397222a53dc5c264ee40c70d4b2e7d34d3
thayer11 <holly.coddington@gmail.com>
2016-07-25 11:46:47 -0600

    passed2tests

--- lib/word.rb
+++ lib/word.rb
@@ -4,6 +4,23 @@ class Word
   end
 
   def original_word
-    return @original_word.downcase
+  	return @original_word.downcase 
   end
+
+  def piglatinize
+	if (original_word.chars.first == "a") || (original_word.chars.first == "e") || (original_word.chars.first == "i") || (original_word.chars.first == "o") || (original_word.chars.first == "u")
+		return original_word + "way"
+	else
+	
+		if (orginal_word.chars[1] ! == "a") || (orginal_word.chars[1] ! == "e") || (orginal_word.chars[1] ! == "i") || (orginal_word.chars[1] ! == "o") (orginal_word.chars[1] ! == "u")
+
+		new_word = original_word.chars
+			new_word.rotate(1).join("") + "ay"
+			
+
+		end
+	end
+   end
 end
+
+
