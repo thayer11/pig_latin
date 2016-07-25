62e2f488bec8eece8a693be0f7fec6f7cda88e16
J.P. Barela <barela@jpbarela.com>
2016-07-25 08:57:01 -0600

    Update solutions.

--- solution/lib/word.rb
+++ solution/lib/word.rb
@@ -8,21 +8,16 @@ class Word
   end
 
   def piglatinize
-    vowels = ["a","e","i","o","u"]
-    # if first letter is a consonant, start a while loop
-    # that goes through each letter of the word
-    # evaluating whether or not the letter in question is a consonant.
-    # If it is, the counter (which essentially represents an index value for the word)
-    # increments. If the loop encounters a vowel, it dies, but the counter lives on.
-    if !vowels.include?(original_word[0]) # first letter is a consonant
-      counter = 0
-      while !vowels.include?(original_word[counter])
-        counter += 1
-      end
-      # returns everything from the first vowel to the end, then the removed consonants, then "ay"
-      return original_word[counter..-1] + original_word[0..(counter-1)] +"ay"
-    else # first letter is a vowel
-      return original_word + "way"
-    end
+  	word_array = original_word.chars
+  	first_letters = []
+  	first_letters.push(word_array.shift) unless ['a', 'e','i','o','u'].include? word_array.first
+  	if first_letters.empty?
+  		original_word + 'way'	
+  	else
+  		while !['a', 'e','i','o','u'].include? word_array.first
+  			first_letters.push(word_array.shift) 
+  		end
+  		word_array.join + first_letters.join + 'ay'
+  	end
   end
 end
